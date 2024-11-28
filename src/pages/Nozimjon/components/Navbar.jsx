ae0837a1f09815648b03163f01fe31133cf5b967
Nozimjon <masteryt123@mail.ru>
2024-11-28 20:46:49 +0500

    start of work

---
+++ src/pages/Nozimjon/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+export default function Navbar() {
+    return(
+        <>
+            <nav>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
