c846f1e504a710dfde3a002f027180e932e88875
Nozimjon <masteryt123@mail.ru>
2024-11-28 23:37:10 +0500

    navbar, header

--- src/pages/Nozimjon/Nozimjon.jsx
+++ src/pages/Nozimjon/Nozimjon.jsx
@@ -1,11 +1,21 @@
 import './Nozimjon.css'
 import './components.css'
 import Navbar from "./components/Navbar"
+import { Link } from 'react-router-dom'
 
 export default function Nozimjon() {
     return(
         <>
             <Navbar />
+            <main>
+                <div className='hidden-gradient w-100 h-100 p-100'>
+                    <div className="w-50">
+                        <h1 className='text-white text-52'>The best IT team <span className='cursiv text-60 textc-cyber-2 textn-cyber-2'>ever</span> !</h1><br />
+                        <p className='text-white'>Our team was founded in 2023, and since then, we've rapidly become one of the most renowned IT companies in the industry. With nearly 20 offices across the globe and a talented team of over 30 collaborators, we are proud of our diverse and dynamic workforce. We've successfully launched a variety of large-scale projects, each making a significant impact in our field. Our commitment to innovation and excellence has positioned us at the forefront of the tech world, and we continue to expand our reach and capabilities every year.</p><br />
+                        <Link className='modern-a' to="">Learn<spam className="space"></spam>more →</Link>
+                    </div>
+                </div>
+            </main>
         </>
     )
 }
--- src/pages/Nozimjon/components/Navbar.jsx
+++ src/pages/Nozimjon/components/Navbar.jsx
@@ -1,17 +1,21 @@
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 import logo from './img/logo.png'
 
 export default function Navbar() {
     return(
         <>
             <nav>
-                <img src={ logo } alt="logo" />
+                <div className='flex aic gap-10'>
+                    <img src={ logo } alt="logo" />
+                    <p className="cyber textc-cyber-1 textn-cyber-1 text-36 textw-700">CYBER</p>
+                </div>
                 <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Donations</a></li>
-                    <li><a href="">Branches</a></li>
-                    <li><a href="">Profile</a></li>
+                    <li><Link to="" className='textc-cyber-1h'>Home</Link></li>
+                    <li><Link to="" className='textc-cyber-1h'>About</Link></li>
+                    <li><Link to="" className='textc-cyber-1h'>Donations</Link></li>
+                    <li><Link to="" className='textc-cyber-1h'>Branches</Link></li>
+                    <li><Link to="" className='textc-cyber-1h'>Profile</Link></li>
                 </ul>
             </nav>
         </>
