4374bb52ad36350f3bb2986e231507e304fe6f7b
Farruh Sadullayev <sadullayevf923@gmail.com>
2024-11-30 19:55:15 +0500

    man projectimni qilib boldim

---
+++ src/pages/Farruh/Farruh.jsx
@@ -0,0 +1,161 @@
+import React from "react";
+import "./Farruh.css";
+
+const Farruh = () => {
+  return (
+    <div className="main-container">
+    
+
+      <nav className="navbar">
+          <ul>
+                    <div className="navbar-logo">PathSoft</div>
+        <ul className="navbar-links"></ul>
+          <li><a href="#ai">AI</a></li>
+          <li><a href="#about">About Company</a></li>
+          <li><a href="#building">Building</a></li>
+          <li><a href="#work">Work</a></li>
+          <li><a href="#relax">Relax</a></li>
+          <li><a href="#global">Global</a></li>
+        </ul>
+      </nav>
+
+<header className="header" id="home">
+  <div className="header-content">
+    <h1>IT Solutions for Your Business</h1>
+    <p>
+      Since our establishment, we have been providing reliable and
+      sustainable software solutions to improve your business process.
+    </p>
+    <div className="header-buttons">
+      <button className="btn-primary">Learn More</button>
+      <button className="btn-secondary">Get in Touch</button>
+    </div>
+  </div>
+</header>
+
+
+<section className="services" id="services">
+  <div className="services-header">
+
+   
+   
+    <h2>Our Services</h2>
+  </div>
+  <div className="services-container">
+          <div className="service-card">
+             <i className="fas fa-cogs phone"></i>
+      <h3>Corporate Solution</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt unde mollitia laboriosam, asperiores nihil dolorem sint cum voluptate suscipit accusamus amet consectetur assumenda reiciendis, omnis vel. Inventore, repellat quis.
+      </p>
+    </div>
+          <div className="service-card">
+             <i className="fas fa-phone phone"></i> 
+      <h3>Call Center Solutions</h3>
+      <p>
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, quisquam tempora molestias sunt voluptates exercitationem corrupti tempore, voluptatibus necessitatibus totam nam. Maxime unde tempora dicta reiciendis velit voluptatem dolor neque.
+      </p>
+    </div>
+          <div className="service-card">
+             <i className="fas fa-download phone"></i>
+      <h3>Cloud Development</h3>
+      <p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt deleniti beatae vitae iste eum. Odit obcaecati libero dolorum, veritatis sequi recusandae qui voluptatem voluptates ipsam earum doloribus harum officia!      </p>
+    </div>
+  </div>
+  <button className="btn-primary">See All Services</button>
+</section>
+
+
+      <section className="why-choose-us" id="about">
+        <h2>Why Choose Us</h2>
+        <div className="features-container">
+          <div className="feature-item">
+            <h3>High Quality Hardware</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus, magni temporibus ex dignissimos adipisci perspiciatis voluptas natus quibusdam magnam nemo consequuntur animi ipsa officiis quis maiores esse? Sed, odio harum?</p>
+          </div>
+          <div className="feature-item">
+            <h3>Dedicated 24/7 Support</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, praesentium. Mollitia expedita, quo ipsa quia quos, aspernatur fuga fugiat dolore porro ullam, pariatur illo doloribus tenetur tempore neque deserunt vero?</p>
+          </div>
+          <div className="feature-item">
+            <h3>30-Day Money-back Guarantee</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas maxime ipsum quasi optio iste distinctio ut? Necessitatibus odio ad quidem. Dolorum tempora ipsam numquam, consequuntur laudantium provident sed aliquam fuga!</p>
+          </div>
+          <div className="feature-item">
+            <h3>Agile and Fast Working Style</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quos esse aspernatur expedita, quis ipsam consectetur aut natus debitis, molestias culpa facilis accusamus dolore iste temporibus voluptatem possimus quibusdam et?</p>
+          </div>
+          <div className="feature-item">
+            <h3>Some Apps are Free</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa facilis a maiores quis ea eos omnis expedita est nam in, reprehenderit, provident eveniet deserunt, tempora suscipit molestiae saepe tenetur accusantium.</p>
+          </div>
+          <div className="feature-item">
+            <h3>High Level of Usability</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate doloribus cum quam quaerat enim a expedita, esse quas facere exercitationem illum inventore aperiam vel ipsum, sit animi ea dicta distinctio?</p>
+          </div>
+        </div>
+      </section>
+  
+      <section className="experience-section">
+        <div className="experience-container">
+          <div className="experience-circle">
+            <div className="circle-content">
+              <h1>10</h1>
+              <p>Years<br />Of Experience</p>
+            </div>
+          </div>
+          <div className="experience-stats">
+            <p><span>2K</span> Apps Developed</p>
+            <p><span>40</span> Consultants</p>
+            <p><span>160</span> Employers</p>
+          </div>
+        </div>
+      </section>
+<footer className="footer">
+  <div className="footer-top">
+    <p>Наши соц-сети</p>
+    <div className="social-icons">
+      <a href="#"><i className="fab fa-facebook"></i></a>
+      <a href="#"><i className="fab fa-twitter"></i></a>
+      <a href="#"><i className="fab fa-google"></i></a>
+      <a href="#"><i className="fab fa-instagram"></i></a>
+      <a href="#"><i className="fab fa-linkedin"></i></a>
+      <a href="#"><i className="fab fa-github"></i></a>
+    </div>
+  </div>
+
+  <div className="footer-content">
+    <div className="footer-column">
+      <h4>IT Solutions for Your Business</h4>
+      <p>      Since our establishment, we have been providing reliable and <br />
+      sustainable software solutions to improve your business process.</p>
+    </div>
+
+    <div className="footer-column">
+      <h4>Полезные ссылки</h4>
+      <ul>
+        <li><a href="#">AI</a></li>
+        <li><a href="#">Building</a></li>
+        <li><a href="#">Work</a></li>
+        <li><a href="#">Relax</a></li>
+        <li><a href="#">Global</a></li>
+      </ul>
+    </div>
+    <div className="footer-column">
+      <h4>Свяжитесь с нами</h4>
+      <ul>
+        <li>Tashkent</li>
+        <li>sadullayevfarruh528@gmail.com</li>
+        <li>+998 90 818 51 51</li>
+        <li>+998 90 818 51 51</li>
+      </ul>
+    </div>
+  </div>
+
+</footer>
+    </div>
+  );
+};
+
+export default Farruh;
