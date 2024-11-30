6ab4438c9f5bd01039496526a3f8f02af887e070
Nozimjon <masteryt123@mail.ru>
2024-11-30 20:37:55 +0500

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Nozimjon from './pages/Nozimjon/Nozimjon';
 import Asad from './pages/Asadbek/Asad'
-
+import Ruxsora from './pages/Ruxsora/Ruxsora';
 
 
 export default function App() {
@@ -9,7 +9,7 @@ export default function App() {
     <Routes>
       <Route path='/' element={<Nozimjon />} />
       <Route path='/asad' element={<Asad />} />
+      <Route path='/ruxsora' element={<Asad />} />
     </Routes>
   )
 }
-
