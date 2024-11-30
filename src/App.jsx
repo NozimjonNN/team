2ec5d1dcf487647c7826aaa4c26eadedf85e81b6
Nozimjon <masteryt123@mail.ru>
2024-11-30 21:30:54 +0500

    intema

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Nozimjon from './pages/Nozimjon/Nozimjon';
 // import Asad from './pages/Asadbek/Asad'
 import Ruxsora from './pages/Ruxsora/Ruxsora';
 import Farruh from './pages/Farruh/Farruh';
+import Diyorbek from './pages/Diyorbek/Diyorbek';
 
 export default function App() {
   return(
     <Routes>
       <Route path='/' element={<Nozimjon />} />
-      {/* <Route path='/asad' element={<Asad />} /> */}
+      <Route path='/asad' element={<Asad />} />
       <Route path='/ruxsora' element={<Ruxsora />} />
       <Route path='/farruh' element={<Farruh />} />
+      <Route path='/farruh' element={<Farruh />} />
+      <Route path='/diyorbek' element={<Diyorbek />} />
     </Routes>
   )
 }
--- src/pages/Asadbek/Asad.jsx
+++ src/pages/Asadbek/Asad.jsx
@@ -1,31 +1,31 @@
-// import React from "react";
-// import Header from "./Header";
-// import Hero from "./Hero";
-// import ProductCard from "./ProductCard";
-// import PromoBanner from "./PromoBanner";
-// import ContactForm from "./ContactForm";
-// import Footer from "./Footer";
-// import "./Asad.css";
+import React from "react";
+import Header from "./Header";
+import Hero from "./Hero";
+import ProductCard from "./ProductCard";
+import PromoBanner from "./PromoBanner";
+import ContactForm from "./ContactForm";
+import Footer from "./Footer";
+import "./Asad.css";
 
-// const Asad = () => {
-//   return (
-//     <div className="App">
-//       <Header />
-//       <Hero />
-//       <section className="products">
-//   <div className="product-row">
-//     <ProductCard image="https://static.wixstatic.com/media/84770f_3d61665c560442849e3ac2f6f7675427~mv2_d_1500_1500_s_2.jpg/v1/fill/w_455,h_648,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/84770f_3d61665c560442849e3ac2f6f7675427~mv2_d_1500_1500_s_2.jpg" title="Christmas Trees" />
-//     <ProductCard image="https://static.wixstatic.com/media/84770f_0a0ede78262c4c5e86861c5c598e82e6~mv2_d_1500_1500_s_2.jpg/v1/fill/w_477,h_648,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/84770f_0a0ede78262c4c5e86861c5c598e82e6~mv2_d_1500_1500_s_2.jpg" title="Unique Ornaments" />
-//     <ProductCard image="https://static.wixstatic.com/media/84770f_8ef70e422ee7477b8237772b4cd69cd3~mv2_d_1500_1500_s_2.jpg/v1/fill/w_455,h_648,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/84770f_8ef70e422ee7477b8237772b4cd69cd3~mv2_d_1500_1500_s_2.jpg" title="Holiday Lights" />
-//   </div>
+const Asad = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <section className="products">
+  <div className="product-row">
+    <ProductCard image="https://static.wixstatic.com/media/84770f_3d61665c560442849e3ac2f6f7675427~mv2_d_1500_1500_s_2.jpg/v1/fill/w_455,h_648,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/84770f_3d61665c560442849e3ac2f6f7675427~mv2_d_1500_1500_s_2.jpg" title="Christmas Trees" />
+    <ProductCard image="https://static.wixstatic.com/media/84770f_0a0ede78262c4c5e86861c5c598e82e6~mv2_d_1500_1500_s_2.jpg/v1/fill/w_477,h_648,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/84770f_0a0ede78262c4c5e86861c5c598e82e6~mv2_d_1500_1500_s_2.jpg" title="Unique Ornaments" />
+    <ProductCard image="https://static.wixstatic.com/media/84770f_8ef70e422ee7477b8237772b4cd69cd3~mv2_d_1500_1500_s_2.jpg/v1/fill/w_455,h_648,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/84770f_8ef70e422ee7477b8237772b4cd69cd3~mv2_d_1500_1500_s_2.jpg" title="Holiday Lights" />
+  </div>
 
-// </section>
+</section>
 
-//       <PromoBanner />
-//       <ContactForm />
-//       <Footer />
-//     </div>
-//   );
-// };
+      <PromoBanner />
+      <ContactForm />
+      <Footer />
+    </div>
+  );
+};
 
-// export default Asad;
+export default Asad;
