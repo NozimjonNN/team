0d012a1a1cb1dfd2a5f777adcf545d6d6f5224e4
Ruxsora <Ruxsora@gmail.com>
2024-11-30 19:22:55 +0500

    ruxsora tugatti

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-
+import React from 'react';
+import Ruxsora from "./pages/Ruxsora/Ruxsora";
 export default function App() {
-
+return(
+    <>
+    <Ruxsora/>
+    </>
+)
 }
---
+++ src/pages/Ruxsora/Ruxsora.jsx
@@ -0,0 +1,156 @@
+import React from "react";
+import "./Ruxsora.css"
+
+
+
+
+const Ruxsora = ()=>{
+    return(
+        <div>
+ <nav>
+          <ul >
+          <li><a style={{color:'black'}}  href="">AI</a></li>
+            <li><a style={{color:'black',marginLeft:'90px'}}  href="">Home</a></li>
+            <li><a style={{color:'black',marginLeft:'70px'}} href="">About</a></li>
+            <li><a style={{color:'black',marginLeft:'70px'}} href="">Register</a></li>
+            <li><a style={{color:'black',marginLeft:'70px'}} href="">Register</a></li>
+            <li><a style={{color:'black',marginLeft:'70px'}} href="">Register</a></li>
+            
+          </ul>
+        </nav>
+
+{/* boshlandi */}
+
+<div style={{display:'flex',justifyContent:'space-around',gap:'60px   ',marginTop:'60px'}} className="container">
+    <div style={{justifyContent:'space-around'}} className="box1">
+<img style={{width:'50%', height:'50%', marginLeft:'100px'}} src="https://ichef.bbci.co.uk/ace/standard/1024/cpsprodpb/14202/production/_108243428_gettyimages-871148930.jpg" alt="" />
+<h1 style={{color:'blakblue',marginLeft:'100px'}}>What is an AI?</h1>
+<p style={{color:'blackblue',marginLeft:'100px'}}>Artificial intelligence (AI) is a <br /> set of technologies that enable computers <br /> to perform a variety of advanced functions, <br /> including the ability to see <br />, understand and translate <br /> spoken and written language, <br /> analyze data, make recommendations, and more.</p>
+</div>
+
+
+
+<div style={{justifyContent:'space-around'}} className="box2">
+<img  style={{width:'80%', height:'50%'}}  src="https://i.pinimg.com/736x/c8/3c/22/c83c22a2d384014447ceab851e130325.jpg" alt="" />
+<h1 style={{color:'blakblue'}}>Can I use AI for free?</h1>
+<p style={{color:'blackblue'}}>AAI is free, as open source.  <br />The thing is that you don't have  the sufficient hardware <br /> to run them on your local systems. <br /> If it's some sort <br /> If it's some sort <br /> If it's some sort</p>
+</div>
+
+
+
+
+
+<div style={{justifyContent:'space-around'}} className="box8">
+<img  style={{width:'80%', height:'50%',marginLeft:'60px'}}  src="https://i.pinimg.com/736x/9b/6d/fa/9b6dfaf64b80f38a26cf5d0f5f017798.jpg" alt="" />
+<h1 style={{color:'blakblue',marginLeft:'60px'}}>What is an AI?</h1>
+<p style={{color:'blackblue',marginLeft:'60px'}}>Artificial intelligence (AI) is a <br /> set of technologies that enable computers <br /> to perform a variety of advanced functions, <br /> including the ability to see <br />, understand and translate <br /> spoken and written language, <br /> analyze data, make recommendations, and more.</p>
+</div>
+</div>
+
+
+
+
+
+<div style={{}} className="container5">
+    <img style={{marginTop:'100px',width:'900px', marginLeft:'300px' }} src="https://drmcnatty.com/wp-content/uploads/2024/10/AI2-1536x981.webp" alt="" />
+
+<h2 style={{textAlign:'center'}}>Artificial Intelligence (AI) in Construction Deficiency Detection</h2>
+<p style={{textAlign:'center'}}>AI plays a critical role in automating the identification of deficiencies in construction projects. By analyzing massive <br /> amounts of data generated during the construction process, AI can detect potential <br /> issues that would otherwise go unnoticed.</p>
+
+<h3 style={{textAlign:'center'}}>1.Data-Driven Inspections: AI-powered tools can analyze data from drones, cameras, and sensors <br /> placed on-site to perform real-time inspections. AI can scan construction elements <br /> such as walls, foundations, or structural components, looking for anomalies <br /> that may indicate flaws or deficiencies. These can include cracks, <br /> deformations, or incorrect installations that would lead to future problems <br /> if left unaddressed.</h3>
+</div>
+
+
+
+
+
+<div style={{}} className="container5">
+    <img style={{marginTop:'100px',width:'900px', marginLeft:'300px' }} src="https://drmcnatty.com/wp-content/uploads/2024/10/Virtual-Reality.jpg" alt="" />
+
+<h2 style={{textAlign:'center'}}>Artificial Intelligence (AI) in Construction Deficiency Detection</h2>
+<p style={{textAlign:'center'}}>VR provides a highly immersive environment for construction stakeholders to identify and <br />address project deficiencies in a virtual space, long before they manifest in the physical world.</p>
+
+<h3 style={{textAlign:'center'}}>2.Data-Driven Inspections: AI-powered tools can analyze data from drones, cameras, and sensors <br /> placed on-site to perform real-time inspections. AI can scan construction elements <br /> such as walls, foundations, or structural components, looking for anomalies <br /> that may indicate flaws or deficiencies. These can include cracks, <br /> deformations, or incorrect installations that would lead to future problems <br /> if left unaddressed.</h3>
+</div>
+
+
+
+<h1 style={{textAlign:'center', marginTop:'40px'}}>Staff Augmentation</h1>
+
+<div style={{display:'flex',justifyContent:'space-around'}} className="container3">
+    <img src="https://drmcnatty.com/wp-content/uploads/2020/04/construction-management.jpg" alt="" />
+<div style={{marginTop:'70px'}} className="box7">
+    <p>
+We have an experienced management team that is excellent at finding and <br /> training our staff. DRMcNatty is dedicated to providing you with team members <br /> who fit your organizations needs and level of experience required. We provide <br /> internal training for our young professionals that cover all aspects of program <br /> management, project management, and project controls. We have a rigorous <br /> interview process that we use for hiring experienced professionals, and promote  <br /> continual education through classes, seminars, and training.</p>
+<h1>We have experts ready to help you:</h1>
+<h4>Project Managers <br />
+Project Schedulers <br />
+Cost Analysts <br />
+Program Managers <br />
+Construction Managers <br />
+Contract Managers</h4>
+</div>
+</div>
+
+
+
+{/* zakoncheno */}
+
+<h1 style={{textAlign:'center',marginTop:'80px'}}>Data Services</h1>
+<div style={{display:'flex',justifyContent:'space-around', marginTop:'70px'}} className="container">
+      <div style={{display:'flex',justifyContent:'space-around'}} className="circle-image">
+        <img style={{borderRadius:'500px'}}
+          src="https://drmcnatty.com/wp-content/uploads/2021/02/dashboard3-400x400.png" // Замените на ваш URL картинки
+          alt="Data Visualization"
+        />
+<div style={{marginLeft:'80px'}} className="box9">\
+<h1>Data Services</h1>
+<p>
+We have the knowledge and experience to help you manage and use your data more effectively. We can <br /> assist you with a wide range of challenges, from designing database solutions that integrate the key data <br /> streams across your organization, to using AI and machine learning to help you predict and plan for future <br /> business needs. Our expert team offers support for installation, configuration, and testing, to ensure your <br /> solution delivers everything you need.</p>
+
+<h1 style={{marginTop:'20px'}}>We specialize in:</h1>
+<p>Database Solutions <br />
+Data Analytics/Visualization <br />
+Enterprise Data Management <br />
+Data Science/AI/ML</p>
+</div>
+
+
+
+
+      </div>
+      
+    </div>
+
+
+
+
+
+    <div className="hero-container">
+      <div className="hero-content">
+        <h1>ИСКУССТВЕННЫЙ ИНТЕЛЛЕКТ - ЭТО КРУТО?</h1>
+        <div className="points">
+          <p>🌀 На данный момент только в Южной Корее ищут 10,000 специалистов в области AI!</p>
+          <p>🌀 Младшие разработчики искусственного интеллекта без опыта могут получать 60,000,000 вон (51,000$) в год. А специалисты со стажем — около 100,000,000 вон (85,000$).</p>
+          <p>🌀 В США младший специалист без опыта получает в среднем 93,000$. А инженер со стажем — больше 140,000$!</p>
+          <p>🌀 Самое главное: зарплата будет расти вместе с твоими навыками.</p>
+        </div>
+        <button className="cta-button">Записаться на ознакомительный вебинар</button>
+      </div>
+      <div className="hero-image">
+        <img src="https://i.pinimg.com/736x/c3/5c/dd/c35cddfe490703f3aece14fde6742df4.jpg" alt="AI Illustration" />
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+        </div>
+    )
+}
+export default Ruxsora;
