0427fd2984426615a05002edd97e3d861656f64b
Nozimjon <masteryt123@mail.ru>
2024-11-30 20:48:50 +0500

    bo'ldim

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Nozimjon from './pages/Nozimjon/Nozimjon';
 import Asad from './pages/Asadbek/Asad'
 import Ruxsora from './pages/Ruxsora/Ruxsora';
-
+import Farruh from './pages/Farruh/Farruh';
 
 export default function App() {
   return(
@@ -11,7 +11,7 @@ export default function App() {
       <Route path='/' element={<Nozimjon />} />
       <Route path='/asad' element={<Asad />} />
       <Route path='/ruxsora' element={<Ruxsora />} />
+      <Route path='/farruh' element={<Farruh />} />
     </Routes>
   )
 }
-
